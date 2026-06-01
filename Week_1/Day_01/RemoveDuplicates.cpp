2ef463a6dcd0707413b781f90a22b9807ab7044c
Bhumik Sharma <bhumikkannu@gmail.com>
2026-06-01 14:08:24 +0530

    Update RemoveDuplicates.cpp

--- Week_1/Day_01/RemoveDuplicates.cpp
+++ Week_1/Day_01/RemoveDuplicates.cpp
@@ -1,10 +1,15 @@
 /* 
 Question : Given an integer array nums sorted in non-decreasing order,
 remove the duplicates in-place such that each unique element appears only once.
-The relative order of the elements should be kept the same.
+The relative order of the elements should be kept the same. Return k unique elements.
+
+Approach : insertIndex (Slow Pointer): Tracks where the next unique element should be placed.
+It starts at 1 because the first element (nums[0]) is always unique.
+
+i (Fast Pointer): Scans the array starting from index 1.
 
 Time Complexity : O(n)
-Space Complexity : O(n)
+Space Complexity : O(1)
 
 */
 
@@ -12,22 +17,19 @@ Space Complexity : O(n)
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.size() == 0) return 0;
+        if (nums.empty()) return 0;
 
-        vector<int> expectedNums;
-        expectedNums.push_back(nums[0]);  // first element is always unique
+        
+        int insertIndex = 1; 
 
         for (int i = 1; i < nums.size(); i++) {
+            
             if (nums[i] != nums[i - 1]) {
-                expectedNums.push_back(nums[i]);  // store unique only
+                nums[insertIndex] = nums[i]; 
+                insertIndex++;               
             }
         }
 
-        // copy back unique elements
-        for (int i = 0; i < expectedNums.size(); i++) {
-            nums[i] = expectedNums[i];
-        }
-
-        return expectedNums.size();  // number of unique elements
+        return insertIndex; 
     }
 };
