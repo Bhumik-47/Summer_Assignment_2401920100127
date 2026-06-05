c3fa9056fe22faee4883cb86d10c9aa0143271cc
Bhumik Sharma <bhumikkannu@gmail.com>
2026-06-05 12:19:41 +0530

    Update Valid palindrome.cpp

--- Week_1/Day_5/Valid palindrome.cpp
+++ Week_1/Day_5/Valid palindrome.cpp
@@ -3,11 +3,11 @@ Question:
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
 Given a string s, return true if it is a palindrome, or false otherwise.
 
-Approach : If given string is empty return true , after that we create another string named b which will contain all the lowercase character which were present in string s (either uppercase or lowercase)
-Hence , we can apply algorithm that check palindrome and therefore return ans in boolean.
+Approach : Apply two-pointer technique directly on the original string s. We just need to skip any non-alphanumeric characters on the and check both characters after converting them into lowercase and so on.
+Return true in the end if the condition satisfy till left < right.
 
 Time Complexity: O(n)
-Space complexity: O(n)
+Space complexity: O(1)
 
  
   
@@ -17,35 +17,27 @@ Space complexity: O(n)
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if (s.empty()) {
-            return true;
-        }
-        
-        string b;
-        
-        for (int i = 0; i < s.size(); i++) {
-            
-            if (isalnum(s[i])) {
-                
-                b.push_back(tolower(s[i]));
-            }
-            
-        }
-        
-        
         int left = 0;
-        int right = b.size() - 1;
+        int right = s.size() - 1;
         
         while (left < right) {
             
-            if (b[left] != b[right]) {
+            while (left < right && !isalnum(s[left])) {
+                left++;
+            }
+            
+            while (left < right && !isalnum(s[right])) {
+                right--;
+            }
+            
+            
+            if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
             
             left++;
             right--;
         }
-        
         
         return true;
     }
