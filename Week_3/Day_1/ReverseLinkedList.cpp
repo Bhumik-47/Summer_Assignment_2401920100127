2f897c498679480d664236402cc7e3d099995416
Bhumik Sharma <bhumikkannu@gmail.com>
2026-06-15 21:51:11 +0530

    Create ReverseLinkedList.cpp

---
+++ Week_3/Day_1/ReverseLinkedList.cpp
@@ -0,0 +1,34 @@
+/*
+Question : Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Approach : Using prev,curr and next pointer , implemented an algorithm to reverse the linked list.
+
+Time complexity : O(n)
+Space complexity : O(1)
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode*prev=NULL;
+        ListNode*curr=head;
+        ListNode*next=NULL;
+        while(curr){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+};
