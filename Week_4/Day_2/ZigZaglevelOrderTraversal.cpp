97883d67b7ca7c00baf595d557ce4989bf28f44d
Bhumik Sharma <bhumikkannu@gmail.com>
2026-06-26 12:11:38 +0000

    done day 2 tasks

---
+++ Week_4/Day_2/ZigZaglevelOrderTraversal.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+     queue<TreeNode*>q;
+        vector<vector<int>>ans;
+        if(root == NULL) return ans;
+        q.push(root);
+        int i=0;
+        while(!q.empty()){
+         vector<int>level;
+         int size = q.size();
+        for(int i=0;i<size;i++){
+            TreeNode* node = q.front();
+            level.push_back(node->val);
+            q.pop();
+            if(node->left)
+            q.push(node->left);
+            if(node->right)
+            q.push(node->right);
+        }
+        if(i%2==0)
+        ans.push_back(level);
+        else{
+        reverse(level.begin(),level.end());
+        ans.push_back(level);
+        }
+        i++;
+        }
+        return ans;
+    }
+};
+/*
+time complexity : O(n)
+space complexity : O(n)
+
+*/
